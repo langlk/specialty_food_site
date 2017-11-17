05a00b58dd7662721a64eb0e096c3d18800dd488
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 10:04:00 -0800

    Add product CRUD functionality to views.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,54 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product saved successfully!"
+      redirect_to products_path
+    else
+      flash[:alert] = @product.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product updated successfully!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = @product.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.reviews.each do |review|
+      review.destroy
+    end
+    @product.destroy
+    flash[:notice] = "Product deleted successfully!"
+    redirect_to products_path
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name,:origin, :cost)
+  end
+end
