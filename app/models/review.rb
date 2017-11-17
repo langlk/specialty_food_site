a4544edb5314c804db8e44f824011131420a5c03
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 15:19:33 -0800

    Update validation for rating.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,9 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :author, :content_body, :rating, :product, presence: true
-  validates :rating, inclusion: { in: 1..5 }
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
   validates :content_body, length: { in: 50..250 }
 end
