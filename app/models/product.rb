c9709bab13ee312e87a75b50ce0414e0ec00beca
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 08:58:59 -0800

    Add recent product scope.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :origin, presence: true
+
+  scope :recent, -> { order(created_at: :desc).limit(3) }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,14 @@ describe Product do
   it { should validate_presence_of :origin }
 
   it { should have_many :reviews }
+
+  describe '.recent' do
+    it "should return the 3 most recently added products" do
+      p1 = Product.create(name: "Test1", cost: 1, origin: "Test1")
+      p2 = Product.create(name: "Test2", cost: 2, origin: "Test2")
+      p3 = Product.create(name: "Test3", cost: 1, origin: "Test3")
+      p4 = Product.create(name: "Test4", cost: 1, origin: "Test4")
+      expect(Product.recent).to eq([p4, p3, p2])
+    end
+  end
 end
