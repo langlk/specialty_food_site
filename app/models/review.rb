4a66d3b6cf17d28aac2ed809fb8a59d542b01b3b
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 08:49:39 -0800

    Add rating validation.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
   validates :author, :content_body, :rating, :product, presence: true
+  validates :rating, inclusion: { in: 1..5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,6 +5,7 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :author }
   it { should validate_presence_of :product }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 
   it { should belong_to :product }
 end
