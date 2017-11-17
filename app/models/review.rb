83285acd9277efbf59e40d5bf3dd19b1fb9a9d2f
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 08:52:45 -0800

    Add content_body length validation.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
 
   validates :author, :content_body, :rating, :product, presence: true
   validates :rating, inclusion: { in: 1..5 }
+  validates :content_body, length: { in: 50..250 }
 end
