7ee8d25addefda2f5c3e89b0cf3cfdd50413962a
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-17 09:09:05 -0800

    Add most-reviewed product scope.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,11 @@ class Product < ActiveRecord::Base
   validates :name, :cost, :origin, presence: true
 
   scope :recent, -> { order(created_at: :desc).limit(3) }
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, products.cost, products.origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .first
+  )}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,4 +16,29 @@ describe Product do
       expect(Product.recent).to eq([p4, p3, p2])
     end
   end
+
+  describe '.most_reviewed' do
+    it "should return the product with the most reviews" do
+      p1 = Product.create(name: "Test1", cost: 1, origin: "Test1")
+      p2 = Product.create(name: "Test2", cost: 2, origin: "Test2")
+      p3 = Product.create(name: "Test3", cost: 1, origin: "Test3")
+
+      p2.reviews.create(
+        author: "Test1",
+        content_body: "Lorem ipsum dolor sit amet, consectetur adipiscing volutpat.",
+        rating: 3
+      )
+      p2.reviews.create(
+        author: "Test2",
+        content_body: "Lorem ipsum dolor sit amet, consectetur adipiscing volutpat.",
+        rating: 4
+      )
+      p1.reviews.create(
+        author: "Test3",
+        content_body: "Lorem ipsum dolor sit amet, consectetur adipiscing volutpat.",
+        rating: 3
+      )
+      expect(Product.most_reviewed).to eq(p2)
+    end
+  end
 end
