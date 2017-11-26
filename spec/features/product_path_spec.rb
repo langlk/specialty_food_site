fd802fbcb76cd8fe411bb53bc3c2f8de8faab19c
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-26 13:39:58 -0800

    Add tests to fully cover product controller.

--- spec/features/product_path_spec.rb
+++ spec/features/product_path_spec.rb
@@ -29,9 +29,53 @@ describe "the product management path" do
     expect(page).to have_no_content("New Product")
   end
 
-  it "displays errors if product is missing fields" do
+  it "removes reviews associated with deleted product" do
+    product = Product.create(name: "New Product", cost: 5, origin: "United States of America")
+    review = product.reviews.create(
+      author: "Test1",
+      content_body: "Lorem ipsum dolor sit amet, consectetur adipiscing volutpat.",
+      rating: 3
+    )
+    visit product_path(product)
+    click_link "Delete"
+    visit products_path
+    expect { Review.find(review.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "displays errors if new product is missing fields" do
     visit new_product_path
     click_on "Create Product"
     expect(page).to have_content("Something went wrong!")
+  end
+
+  it "displays errors if updated product is missing fields" do
+    product = Product.create(name: "New Product", cost: 5, origin: "United States of America")
+    visit edit_product_path(product)
+    fill_in "Name", with: ""
+    click_on "Update Product"
+    expect(page).to have_content("Something went wrong!")
+  end
+
+  it "displays only US-made products if selected" do
+    product = Product.create(name: "New Product 1", cost: 5, origin: "United States of America")
+    product = Product.create(name: "New Product 2", cost: 5, origin: "Canada")
+    visit products_path
+    select "US Products Only", from: "method"
+    click_on "Apply"
+    expect(page).to have_no_content("New Product 2")
+    expect(page).to have_content("New Product 1")
+  end
+
+  it "displays the most recent products on the landing page" do
+    product = Product.create(name: "New Product 1", cost: 5, origin: "United States of America")
+    product = Product.create(name: "New Product 1.5", cost: 5, origin: "United States of America")
+    product = Product.create(name: "New Product 2", cost: 5, origin: "Canada")
+    product = Product.create(name: "New Product 3", cost: 5, origin: "Canada")
+    product = Product.create(name: "New Product 4", cost: 5, origin: "Canada")
+    visit root_path
+    expect(page).to have_content("New Product 4")
+    expect(page).to have_content("New Product 3")
+    expect(page).to have_content("New Product 2")
+    expect(page).to have_no_content("New Product 1.5")
   end
 end
